ecfeaa17cef73d0370277b72098f15b23f94bc11
Thiago Paiva <thi.oli.pai@gmail.com>
2021-11-09 20:56:59 -0300

    pronto

---
+++ MIcroondas.js
@@ -0,0 +1,77 @@
+function resultadoDoPrato(comida, segundos){
+
+    if(comida === 'pipoca' && segundos === 10){
+        resultadoDoPrato = 'Prato pronto, bom apetite!!!'
+    }
+    
+    if(comida === 'pipoca' && segundos ==> 20){
+        resultadoDoPrato = 'comida queimou!'
+    }
+
+    if(comida === 'pipoca' && segundos ==> 30){
+        resultadoDoPrato = 'Kabuuum!!!'
+    }
+
+    if(comida === 'pipoca' && segundos ==< 10){
+        resultadoDoPrato = 'tempo insuficiente!'
+
+        if(comida === 'macarrão' && segundos === 8){
+            resultadoDoPrato = 'Prato pronto, bom apetite!!!'
+        }
+        
+        if(comida === 'macarrão' && segundos ==> 16){
+            resultadoDoPrato = 'comida queimou!'
+        }
+    
+        if(comida === 'macarrão' && segundos ==> 24){
+            resultadoDoPrato = 'Kabuuum!!!'
+        }
+    
+        if(comida === 'macarrão' && segundos ==< 8){
+            resultadoDoPrato = 'tempo insuficiente!'
+
+    if(comida === 'carne' && segundos === 15){
+        resultadoDoPrato = 'Prato pronto, bom apetite!!!'
+    }
+    
+    if(comida === 'carne' && segundos ==> 30){
+        resultadoDoPrato = 'comida queimou!'
+    }
+
+    if(comida === 'carne' && segundos ==> 45){
+        resultadoDoPrato = 'Kabuuum!!!'
+    }
+
+    if(comida === 'carne' && segundos ==< 15){
+        resultadoDoPrato = 'tempo insuficiente!'
+
+        if(comida === 'feijão' && segundos === 12){
+            resultadoDoPrato = 'Prato pronto, bom apetite!!!'
+        }
+        
+        if(comida === 'feijão' && segundos ==> 24){
+            resultadoDoPrato = 'comida queimou!'
+        }
+    
+        if(comida === 'feijão' && segundos ==> 36){
+            resultadoDoPrato = 'Kabuuum!!!'
+        }
+    
+        if(comida === 'feijão' && segundos ==< 12){
+            resultadoDoPrato = 'tempo insuficiente!'
+
+if(comida === 'brigadeiro' && segundos === 8){
+        resultadoDoPrato = 'Prato pronto, bom apetite!!!'
+    }
+    
+    if(comida === 'brigadeiro' && segundos ==> 16){
+        resultadoDoPrato = 'comida queimou!'
+    }
+
+    if(comida === 'brigadeiro' && segundos ==> 24){
+        resultadoDoPrato = 'Kabuuum!!!'
+    }
+
+    if(comida === 'pipoca' && segundos ==< 8){
+        resultadoDoPrato = 'tempo insuficiente!'
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Digital-House---Checkpoint1
+
+Prof, sei que está errado e não utlizei diversas coisas ensinadas em aula. Estou enviando apenas para não ficar como "não enviado". Tenho assistido as aulas e acompanhado o playground, mas realmente não consegui resolver o problema proposto. 
+
+Valeu, abraço
+Thiago
